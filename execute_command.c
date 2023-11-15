e4cd24a5a2f38d2c5558e85da8bfe2e89ccb92be
aymanemontassir <aymane-hana@hotmail.fr>
2023-11-15 02:03:23 +0100

    adding tokenizer to add arguments

--- execute_command.c
+++ execute_command.c
@@ -4,41 +4,31 @@
  * execute_command - Executes a command
  * @command: The command to execute
  */
-void execute_command(char *command)
-{
-	pid_t child_pid;
-	char *argv[2];
+void execute_command(char *command) {
+    pid_t child_pid;
+    char *argv[128];
 
-	argv[0] = command;
-	argv[1] = NULL;
+    tokenize_command(command, argv);
 
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid == 0)
-	{
-		/* Child process */
-		if (command[0] == '/' || command[0] == '.')
-		{
-			/* Absolute or relative path */
-			execute_directly(command, argv);
-		}
-		else
-		{
-			/* Search PATH */
-			execute_from_path(command, argv);
-		}
+    child_pid = fork();
+    if (child_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (child_pid == 0) {
+        /* Child process */
+        if (argv[0][0] == '/' || argv[0][0] == '.') {
+            /* Absolute or relative path */
+            execute_directly(argv[0], argv);
+        } else {
+            /* Search PATH */
+            execute_from_path(argv[0], argv);
+        }
 
-		perror(command);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		/* Parent process */
-		int status;
-		waitpid(child_pid, &status, 0);
-	}
+        perror(argv[0]);
+        exit(EXIT_FAILURE);
+    } else {
+        /* Parent process */
+        int status;
+        waitpid(child_pid, &status, 0);
+    }
 }
