88d36dcce8ed623ef3a920780fd544add48df418
aymanemontassir <aymane-hana@hotmail.fr>
2023-11-29 04:01:20 +0100

    new update : trying to fix

--- execute_command.c
+++ execute_command.c
@@ -5,101 +5,30 @@
  * @command: The command to execute
  */
 void execute_command(char *command) {
-<<<<<<< HEAD
-	pid_t child_pid;
-	char *argv[128];
-
-	tokenize_command(command, argv);
-=======
     pid_t child_pid;
     char *argv[128];
-    char *cmd_copy = strdup(command);
-    int cmd_exists = 0;
 
-    tokenize_command(cmd_copy, argv);
+    tokenize_command(command, argv);
 
-    if (strcmp(argv[0], "env") == 0)
-    {
-        print_env();
-        free(cmd_copy);
-        return;
-    }
-
-    if (argv[0][0] == '/' || argv[0][0] == '.') {
-        /* Absolute or relative path */
-        if (access(argv[0], X_OK) == 0) {
-            cmd_exists = 1;
+    child_pid = fork();
+    if (child_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (child_pid == 0) {
+        /* Child process */
+        if (argv[0][0] == '/' || argv[0][0] == '.') {
+            /* Absolute or relative path */
+            execute_directly(argv[0], argv);
+        } else {
+            /* Search PATH */
+            execute_from_path(argv[0], argv);
         }
-    } else {
-        /* Search PATH */
-        char *path = getenv("PATH");
-        char *path_copy = strdup(path);
-        char *dir = strtok(path_copy, ":");
-
-        while (dir) {
-            char *cmd_path = malloc(strlen(dir) + strlen(argv[0]) + 2);
-            strcpy(cmd_path, dir);
-            strcat(cmd_path, "/");
-            strcat(cmd_path, argv[0]);
-
-            if (access(cmd_path, X_OK) == 0) {
-                cmd_exists = 1;
-                free(cmd_path);
-                break;
-            }
-
-            free(cmd_path);
-            dir = strtok(NULL, ":");
-        }
-
-        free(path_copy);
-    }
-
-    if (!cmd_exists) {
-        fprintf(stderr, "%s: command not found\n", argv[0]);
-        free(cmd_copy);
-        return;
-    }
->>>>>>> 9f62f074df3f143afcfe9d15deb0547488faafc3
-
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid == 0) 
-	{
-		/* Child process */
-		if (argv[0][0] == '/' || argv[0][0] == '.')
-		{
-			/* Absolute or relative path */
-			execute_directly(argv[0], argv);
-        }
-		else
-		{
-			/* Search PATH */
-			execute_from_path(argv[0], argv);
-		}
 
         perror(argv[0]);
         exit(EXIT_FAILURE);
-<<<<<<< HEAD
-	}
-	else
-	{
-		/* Parent process */
-		int status;
-		waitpid(child_pid, &status, 0);
-	}
-}
-=======
     } else {
         /* Parent process */
         int status;
         waitpid(child_pid, &status, 0);
     }
-
-    free(cmd_copy);
 }
->>>>>>> 9f62f074df3f143afcfe9d15deb0547488faafc3
--- tokenize_command.c
+++ tokenize_command.c
@@ -8,30 +8,10 @@
 void tokenize_command(char *command, char **argv)
 {
     int i = 0;
-    char *token_start = command;
 
-    while (1)
-    {
-        char c = *command;
-
-        if (c == ' ' || c == '\0')
-        {
-            size_t length = command - token_start;
-            char *token = malloc(length + 1);
-            strncpy(token, token_start, length);
-            token[length] = '\0';
-            argv[i++] = token;
-
-            if (c == '\0')
-            {
-                break;
-            }
-
-            token_start = command + 1;
-        }
-
-        command++;
+    argv[i] = strtok(command, " ");
+    while (argv[i] != NULL) {
+        i++;
+        argv[i] = strtok(NULL, " ");
     }
-
-    argv[i] = NULL;
 }
