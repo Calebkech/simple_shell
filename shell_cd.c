e511a3c50dbb8bc4531d8867a15aab94324cdca6
calebkech <calebkemboi61@gmail.com>
2023-12-01 17:11:43 +0300

    cd update

--- shell_cd.c
+++ shell_cd.c
@@ -1,26 +1,51 @@
 #include "shell.h"
 
 /**
- * shell_getenv - get the value of an environment variable
- * @name: name of the environment variable
- * @envp: pointer to the environment variables
- * Return: char* with the matching path
+ * shell_cd - Change the current directory of the process.
+ * @args: Array of command arguments (args[0] is the command itself).
+ * Return: Always returns 1 to continue the shell loop.
  */
-
 int shell_cd(char **args)
 {
+    char *new_dir;
     int check;
 
-    if (args[1] == NULL)
+    if (args[1] == NULL || strcmp(args[1], "~") == 0)
     {
-        puts("Expected argument to cd");
-        return 1;
+        /* If no argument is given or if "~" is given, change to the home directory */
+        new_dir = getenv("HOME");
+        if (new_dir == NULL)
+        {
+            perror("Error retrieving home directory");
+            return 1;
+        }
+    }
+    else if (strcmp(args[1], "-") == 0)
+    {
+        /* If the argument is "-", change to the previous directory */
+        new_dir = getenv("OLDPWD");
+        if (new_dir == NULL)
+        {
+            perror("Error retrieving previous directory");
+            return 1;
+        }
+    }
+    else
+    {
+        /* Change to the specified directory */
+        new_dir = args[1];
     }
 
-    check = chdir(args[1]);
+    check = chdir(new_dir);
     if (check != 0)
     {
-        perror("Error:");
+        perror("Error changing directory");
+    }
+    else
+    {
+        /* Update the PWD and OLDPWD environment variables */
+        setenv("OLDPWD", getenv("PWD"), 1);
+        setenv("PWD", new_dir, 1);
     }
 
     free(args); /* Perform the free operation here, after the last usage of args */
